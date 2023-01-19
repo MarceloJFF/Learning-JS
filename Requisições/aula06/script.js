62ed51edb5677143814c0d38b51a398a512baa3e
Marcelo Jhones <marceloj@acad.ifma.edu.br>
2023-01-19 02:28:29 -0300

    utilização do async await para fazer requisições

--- Requisições/aula06/script.js
+++ Requisições/aula06/script.js
@@ -4,46 +4,56 @@ document.querySelector('button').addEventListener('click',
 
 document.querySelector('#inserir').addEventListener('click',inserir)
 
-//get
-function clicou(){
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(
-        (response) => {
-            return response.json() //transformando para objeto json
-        }
-    ).then( 
-        (data)=>{
-            console.log(data)
-        }
-    ).catch(
-        (error)=>{
-        console.log(error)
-    }).finally(()=>{
-        alert("Requisicao encerrada")
-    })
-    alert("CLicou")// essa parte aparece primeiro devido a assincronicidade do JS
+async function clicou(){
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    let json = await  response.json();
+    let data = json;
+
+    console.log(json)
 }
 
 
-//post
-function inserir(){
-    fetch( ('https://jsonplaceholder.typicode.com/posts'),
-    {
-        method: 'POST',
-        headers: {
-            'Content-Type':'application/json'
-        },
-        body: JSON.stringify(
-            {
-                title: 'Titulo de teste',
-                body: 'Corpo de teste',
-                userId:2
-            }
-        )
 
-    }).then( (response)=>{
-        return response.json()
-    }).then((json)=>{
-        console.log(json)
-    })
-}
+// //get
+// function clicou(){
+//     fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(
+//         (response) => {
+//             return response.json() //transformando para objeto json
+//         }
+//     ).then( 
+//         (data)=>{
+//             console.log(data)
+//         }
+//     ).catch(
+//         (error)=>{
+//         console.log(error)
+//     }).finally(()=>{
+//         alert("Requisicao encerrada")
+//     })
+//     alert("CLicou")// essa parte aparece primeiro devido a assincronicidade do JS
+// }
+
+
+// //post
+// function inserir(){
+//     fetch( ('https://jsonplaceholder.typicode.com/posts'),
+//     {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type':'application/json'
+//         },
+//         body: JSON.stringify(
+//             {
+//                 title: 'Titulo de teste',
+//                 body: 'Corpo de teste',
+//                 userId:2
+//             }
+//         )
+
+//     }).then( (response)=>{
+//         return response.json()
+//     }).then((json)=>{
+//         console.log(json)
+//     })
+// }
